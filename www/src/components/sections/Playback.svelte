ac3e1b0a497ae04906bf8e0dff52cc6bc2cbcc71
Braden Wiggins <braden@fractal-hq.com>
2024-05-30 15:31:33 -0400

    feat: nicer playback example

--- www/src/components/sections/Playback.svelte
+++ www/src/components/sections/Playback.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { fadeOutUp, fadeInUp, fadeText } from '../../utils/animations.ts'
 	import { PocketShader } from 'pocket-shader'
 
 	export let id: string
@@ -6,6 +7,8 @@
 	let ps: PocketShader | null = null
 	let state = ''
 	let t = ''
+	let stateTextEl: HTMLDivElement
+	let btnEl: HTMLButtonElement
 
 	$: disabled = !state || state === 'disposed' || ps === null
 
@@ -21,9 +24,11 @@
 		})
 
 		update()
+		fadeText(btnEl, 'Dispose')
 	}
 
 	function dispose() {
+		fadeText(btnEl, 'Run')
 		ps?.dispose()
 		ps = null
 		update()
@@ -45,29 +50,50 @@
 		update()
 	}
 
-	function update() {
+	async function update() {
+		await fadeOutUp(stateTextEl)
 		state = ps?.state ?? ''
+		fadeInUp(stateTextEl)
 	}
 </script>
 
 <slot name="code" />
 
-<button class="btn" class:active={!disabled} on:click={run}>{disabled ? 'Run' : 'Dispose'}</button>
-
-<div class="kv" class:disabled>
-	<div class="key"><code>ps.state</code></div>
-	<div class="value" class:disabled>{state}</div>
-</div>
-
-<div class="kv" class:disabled>
-	<div class="key"><code>ps.time</code></div>
-	<div class="value" class:disabled>{t}</div>
-</div>
+<button bind:this={btnEl} class="btn" class:active={!disabled} on:click={run}>Run</button>
 
 <div class="flex">
-	<button class="code gradient-outline" {disabled} on:click={start}>ps.start()</button>
-	<button class="code gradient-outline" {disabled} on:click={pause}>ps.pause()</button>
-	<button class="code gradient-outline" {disabled} on:click={stop}>ps.stop()</button>
+	<button
+		class="code"
+		class:gradient-outline={!disabled && state !== 'running'}
+		class:active={state === 'running'}
+		disabled={disabled || state === 'running'}
+		on:click={start}>ps.start()</button
+	>
+	<button
+		class="code"
+		class:gradient-outline={!disabled && state !== 'paused'}
+		class:active={state === 'paused'}
+		disabled={disabled || state !== 'running'}
+		on:click={pause}>ps.pause()</button
+	>
+	<button
+		class="code"
+		class:gradient-outline={!disabled && state !== 'stopped'}
+		class:active={state === 'stopped'}
+		disabled={disabled || state !== 'running'}
+		on:click={stop}>ps.stop()</button
+	>
+</div>
+
+<div>
+	<div class="kv" class:disabled>
+		<div class="key"><code>ps.state</code></div>
+		<div class="value" class:disabled bind:this={stateTextEl}>{state}</div>
+	</div>
+	<div class="kv" class:disabled>
+		<div class="key"><code>ps.time</code></div>
+		<div class="value" class:disabled>{t}</div>
+	</div>
 </div>
 
 <style lang="scss">
@@ -77,10 +103,20 @@
 		justify-content: space-between;
 		width: 11rem;
 		margin: auto;
-		outline: var(--outline);
-		border-radius: var(--radius);
+		// outline: var(--outline);
+		border-radius: 0.15rem;
 		overflow: hidden;
 		height: 1.75rem;
+
+		&:first-of-type {
+			border-bottom-left-radius: 0;
+			border-bottom-right-radius: 0;
+		}
+
+		&:last-of-type {
+			border-top-left-radius: 0;
+			border-top-right-radius: 0;
+		}
 
 		div {
 			display: flex;
@@ -89,6 +125,16 @@
 			flex: 1fr;
 
 			line-height: 1.75rem;
+		}
+
+		.key,
+		.key code {
+			border-radius: 0;
+			width: 5.5rem;
+			font-variation-settings: 'wght' 100;
+			color: var(--fg-b);
+			background: none;
+			outline: none;
 		}
 
 		.value {
@@ -103,23 +149,39 @@
 		margin: auto;
 	}
 
-	button.code {
-		color: var(--fg-a);
-		font-variation-settings: 'wght' 300;
-		font-size: var(--font-sm);
-		position: relative;
-
-		padding: 0.25rem 0.5rem;
-	}
-
 	.disabled,
 	button:disabled {
 		opacity: 0.5;
-		filter: grayscale(1) brightness(0.5);
-		cursor: not-allowed;
+		// filter: grayscale(1) brightness(0.5);
+		cursor: default;
+		pointer-events: none;
 
 		&::before {
 			display: none;
+		}
+	}
+
+	button.code {
+		color: var(--fg-a) !important;
+		font-variation-settings: 'wght' 300;
+		font-size: var(--font-sm);
+		position: relative;
+		background: var(--bg-a);
+		border-radius: 0.5rem;
+
+		padding: 0.25rem 0.5rem;
+
+		&.active {
+			// color: var(--bg-a);
+			// --theme-a: #30d84c;
+			// --theme-b: #ff0051;
+			// background-position: 25% 50%;
+			// background: linear-gradient(
+			// 	var(--deg, 45deg),
+			// 	var(--theme-a, #33c5f4),
+			// 	var(--theme-b, #a269cb) 100%
+			// );
+			// outline: var(--gradient);
 		}
 	}
 </style>
