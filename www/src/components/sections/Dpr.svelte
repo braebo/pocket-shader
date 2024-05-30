83826ce5797cb2e02700eb634f8515dd8ef31ad7
Braden Wiggins <braden@fractal-hq.com>
2024-05-30 15:31:20 -0400

    feat: made a mess for some nicer styles / anims

--- www/src/components/sections/Dpr.svelte
+++ www/src/components/sections/Dpr.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import fragmentShader from '../../shaders/dyingUniverse.glsl?raw'
+	import { fadeText } from '../../utils/animations.ts'
 	import { PocketShader } from 'pocket-shader'
 	import Range from '../Range.svelte'
 
@@ -7,6 +8,7 @@
 	export let id: string
 
 	let ps: PocketShader | null = null
+	let btnEl: HTMLButtonElement
 
 	$: if (dpr && ps) {
 		ps.maxPixelRatio = dpr
@@ -22,76 +24,23 @@
 			maxPixelRatio: dpr,
 			autoStart: true,
 		})
+
+		fadeText(btnEl, 'Dispose')
 	}
 
 	function dispose() {
 		ps?.dispose()
 		ps = null
+		fadeText(btnEl, 'Run')
 	}
 </script>
 
 <slot name="code" />
 
-<button class="btn" class:active={!disabled} on:click={run}>{disabled ? 'Run' : 'Dispose'}</button>
+<button class="btn" bind:this={btnEl} class:active={!disabled} on:click={run}>
+	{disabled ? 'Run' : 'Dispose'}
+</button>
 
 <p><code>{dpr.toFixed(1)}</code></p>
 
 <Range min={0.1} max={2} bind:value={dpr} />
-
-<!-- <input type="range" min="1" max="20" bind:value={dpr} /> -->
-
-<!-- 
-<style lang="scss">
-	input:disabled {
-		opacity: 0.5;
-		filter: grayscale(1) brightness(0.5);
-		cursor: not-allowed;
-	}
-
-	$height: 1rem;
-
-	input[type='range'] {
-		-webkit-appearance: none;
-		width: 20rem;
-		height: $height;
-		margin: auto;
-		
-		transition: opacity 0.2s;
-		border-radius: $height;
-	}
-
-	@mixin thumb {
-		-webkit-appearance: none;
-		appearance: none;
-		width: $height / 4;
-		height: $height;
-		background: var(--theme-a);
-		cursor: pointer;
-		transform: translateY(-1px);
-		z-index: 1;
-	}
-
-	@mixin track {
-		width: 100%;
-		height: $height;
-		cursor: pointer;
-		background-color: var(--bg-c);
-		border-radius: $height;
-		border-radius: 0.5rem;
-		outline: 1px solid var(--bg-a);
-		border: 1px solid var(--bg-a);
-	}
-
-	input[type='range']::-webkit-slider-thumb {
-		@include thumb;
-	}
-	input[type='range']::-moz-range-thumb {
-		@include thumb;
-	}
-	input[type='range']::-webkit-slider-runnable-track {
-		@include track;
-	}
-	input[type='range']::-moz-range-track {
-		@include track;
-	}
-</style> -->
