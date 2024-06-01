ff989d7f844c44070085ae9c17443e475596fafc
Braden Wiggins <braden@fractal-hq.com>
2024-06-01 18:56:03 -0400

    wip: retro test

--- www/src/components/Retro.svelte
+++ www/src/components/Retro.svelte
@@ -1,22 +1,32 @@
 <script lang="ts">
-    import fragment from '../shaders/retro.glsl?raw'
-    import { PocketShader } from 'pocket-shader';
-    import { onMount } from 'svelte';
+	import fragment from '../shaders/retro.glsl?raw'
+	import { PocketShader } from 'pocket-shader'
+	import { onMount } from 'svelte'
 
-    onMount(() => {
-        const pocketShader = new PocketShader({
-            shadertoy: true,
-            fragment
-        })
-    });
+	onMount(() => {
+		const ps = new PocketShader({
+			uniforms: {
+				u_timeDelta: { type: 'float', value: 0 },
+				// u_audioData: { type: 'float32Array', value: new Float32Array(16) },
+			},
+			fragment,
+		})
+
+		ps.on('render', ({ delta }) => {
+			ps.uniforms.u_timeDelta.value = delta
+		})
+	})
 </script>
 
 <div>
-    <h1>Retro</h1>
+	<h1>Retro</h1>
 
-    <div id="retro-canvas"></div>
+	<div id="retro-canvas"></div>
 </div>
 
 <style lang="scss">
-    
+	#retro-canvas {
+		width: 100%;
+		height: 100vh;
+	}
 </style>
