34eb56d45c83ec89d9e207299348460e0732e151
Braden Wiggins <braden@fractal-hq.com>
2024-06-05 14:10:13 -0400

    feat: nicer button anim

--- www/src/utils/animations.ts
+++ www/src/utils/animations.ts
@@ -4,10 +4,10 @@ export async function fadeOutUp(el: Element) {
 	return await el.animate(
 		[
 			{ transform: 'translateY(0)', opacity: 1 },
-			{ transform: 'translateY(-0.5rem)', opacity: 0 },
+			{ transform: 'translateY(-0.2rem)', opacity: 0 },
 		],
 		{
-			duration: 150,
+			duration: 200,
 			easing: easeOut,
 			fill: 'forwards',
 		},
@@ -17,17 +17,20 @@ export async function fadeOutUp(el: Element) {
 export async function fadeInUp(el: Element) {
 	return await el.animate(
 		[
-			{ transform: 'translateY(0.25rem)', opacity: 0 },
+			{ transform: 'translateY(0.2rem)', opacity: 0 },
 			{ transform: 'translateY(0)', opacity: 1 },
 		],
 		{
-			duration: 150,
+			duration: 200,
 			easing: easeOut,
 			fill: 'forwards',
 		},
 	).finished
 }
 
+/**
+ * Swaps an element's text with a fade animation.
+ */
 export async function fadeText(el: Element, text: string) {
 	const elText = el.textContent
 	const wrapper = document.createElement('div')
