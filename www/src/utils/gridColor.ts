ccfccfcf2d4fd0211510a75c086a729d1049ed2c
Braden Wiggins <braden@fractal-hq.com>
2024-06-05 14:10:23 -0400

    feat: grid state

---
+++ www/src/utils/gridColor.ts
@@ -0,0 +1,15 @@
+import { quintOut } from 'svelte/easing'
+import { tweened } from 'svelte/motion'
+
+export const gridColors = {
+	greyscale: [0.1, 0.1, 0.1],
+	purple: [0.57, 0.23, 1],
+	cyan: [0.2, 0.77, 0.96],
+	red: [1, 0.23137254901960785, 0.5254901960784314],
+    blue: [0.27450980392156865, 0.5215686274509804, 0.9254901960784314],
+}
+
+export const gridColor = tweened(gridColors.greyscale, {
+	duration: 1500,
+	easing: quintOut,
+})
