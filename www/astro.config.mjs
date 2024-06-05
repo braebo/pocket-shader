46fdc46cde42a7eb8b99fb1629f7ba7ce26cfc1f
Braden Wiggins <braden@fractal-hq.com>
2024-06-05 18:39:19 -0400

    feat: bump deps, prune deps/config, re-patch astro

--- www/astro.config.mjs
+++ www/astro.config.mjs
@@ -1,14 +1,11 @@
-import { defineConfig } from 'astro/config';
-
-/** @type {import('astro/config')} */
-import solid from "@astrojs/solid-js";
-
-import svelte from "@astrojs/svelte";
+import { defineConfig } from 'astro/config'
+import svelte from '@astrojs/svelte'
 
 // https://astro.build/config
+/** @type {import('astro/config')} */
 export default defineConfig({
-  vite: {
-    plugins: []
-  },
-  integrations: [solid(), svelte()]
-});
+	vite: {
+		plugins: [],
+	},
+	integrations: [svelte()],
+})
