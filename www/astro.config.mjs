11f88d8fa58e89309bccc321216428c4bdf9b490
Braden Wiggins <braden@fractal-hq.com>
2024-06-05 23:14:24 -0400

    fix: remove cloudflare adapter (it was a cache issue)

--- www/astro.config.mjs
+++ www/astro.config.mjs
@@ -1,9 +1,6 @@
-import cloudflare from '@astrojs/cloudflare'
 import { defineConfig } from 'astro/config'
 import svelte from '@astrojs/svelte'
 
 export default defineConfig({
-	output: 'server',
-	adapter: cloudflare(),
 	integrations: [svelte()],
 })
