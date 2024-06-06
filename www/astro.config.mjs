09b0f5a91861c00311f1d33fefee29e97e8347da
Braden Wiggins <braden@fractal-hq.com>
2024-06-06 18:31:34 -0400

    feat: favicons

--- www/astro.config.mjs
+++ www/astro.config.mjs
@@ -2,5 +2,10 @@ import { defineConfig } from 'astro/config'
 import svelte from '@astrojs/svelte'
 
 export default defineConfig({
+	vite: {
+		define: {
+			__DEV__: JSON.stringify(process.env.NODE_ENV === 'development'),
+		},
+	},
 	integrations: [svelte()],
 })
